3ef07ec14dbd94352beeae3bec20d32f29830e76
tom.touquet <tom.touquet@localhost.localdomain>
2017-10-30 13:03:00 +0100

    final-leap-year

--- is_leap_year.c
+++ is_leap_year.c
@@ -7,14 +7,14 @@
 
 int is_leap_year(int year)
 {
-	if (year % 4 != 0 || year % 400 != 0) {
-		return (0);
-	}
-	if ( year % 4 == 0 && year % 100 != 0 && year % 400 == 0) {
-		return (1);
-	}
-}
 	
-		
-		
-		
+	if (year % 4 != 0) 
+		return (0);
+	else if (year % 100 != 0)
+		return (1);
+	else if (year % 400 != 0)
+		return(0);
+	else
+		return(1);
+
+}
--- test.c
+++ test.c
@@ -4,5 +4,20 @@ int is_leap_year(int year);
 
 Test(simple, 2016)
 {
-	cr_assert(is_leap_year(2016));
+	cr_assert(is_leap_year(2016) == 1);
 }
+
+Test(simple, 2015)
+{
+        cr_assert(is_leap_year(2015) == 0);
+}
+Test(simple, 2030)
+{
+	cr_assert(is_leap_year(2030) == 0);
+}
+Test(simple, 2000)
+{
+	cr_assert(is_leap_year(2000) == 1);
+}
+		  
+
